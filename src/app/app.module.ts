031f5721686bbf1ab3aae7b547ebbbbb15e952cc
erikasantanaa <erikamariasantanaa@gmail.com>
2023-05-13 20:46:01 -0300

    organizando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
-import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
-import { MeuTerceiroComponent } from './meu-terceiro/meu-terceiro.component';
-import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent,
-    MeuSegundoComponent,
-    MeuTerceiroComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
