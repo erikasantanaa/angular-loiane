44d2ee0831ee0c5fd180b4c38865aca163509c77
erikasantanaa <erikamariasantanaa@gmail.com>
2023-05-14 19:34:52 -0300

    Criando primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MeuPrimeiro2Component,
+    MeuPrimeiroComponent,
   ],
   imports: [
     BrowserModule,
