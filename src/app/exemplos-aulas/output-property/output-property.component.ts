b2edf33559156f7f7782411b31c8fbc5464c819a
Erika Santana <51144321+erikasantanaa@users.noreply.github.com>
2023-06-20 15:50:40 -0300

    feat: acessando ao DOM e ao template com ViewChild

--- src/app/exemplos-aulas/output-property/output-property.component.ts
+++ src/app/exemplos-aulas/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-output-property',
@@ -10,12 +10,19 @@ export class OutputPropertyComponent {
 
  @Output() mudouValor = new EventEmitter();
 
-  incrementa() {
+ @ViewChild('campoInput') campoValorInput = ElementRef;
+
+ incrementa() {
+    //acessando ao DOM e ao Template com ViewChild
+    // console.log('valor input', this.campoValorInput.nativeElement.value);
+    // this.campoValorInput.nativeElement.value++
+
     this.valor++;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa() {
+     // this.campoValorInput.nativeElement.value--
     this.valor--;
     this.mudouValor.emit({ novoValor: this.valor });
   }
