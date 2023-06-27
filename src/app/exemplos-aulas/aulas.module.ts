f457d9694ca03d95f84e81dbee89cd4cf3bbbf5c
Erika Santana <51144321+erikasantanaa@users.noreply.github.com>
2023-06-27 15:54:27 -0300

    feat: aula diretiva ngswitch

--- src/app/exemplos-aulas/aulas.module.ts
+++ src/app/exemplos-aulas/aulas.module.ts
@@ -7,6 +7,7 @@ import { InputPropertyComponent } from './input-property/input-property.componen
 import { OutputPropertyComponent } from './output-property/output-property.component';
 import { CicloVidaComponent } from './ciclo-vida/ciclo-vida.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
     InputPropertyComponent,
     OutputPropertyComponent,
     CicloVidaComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/exemplos-aulas/diretiva-ngswitch/diretiva-ngswitch.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngswitch',
+  templateUrl: './diretiva-ngswitch.component.html',
+  styleUrls: ['./diretiva-ngswitch.component.css']
+})
+export class DiretivaNgswitchComponent implements OnInit {
+
+  aba = 'home';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
