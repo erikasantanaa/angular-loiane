95e32b19fc3e9f2e099eaece96904b9714eb8ee7
Erika Santana <51144321+erikasantanaa@users.noreply.github.com>
2023-06-30 11:41:24 -0300

    feat: aulas ngClass, ngStyle, operador elvis

--- src/app/exemplos-aulas/ngclass/ngclass.component.ts
+++ src/app/exemplos-aulas/ngclass/ngclass.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./ngclass.component.css']
 })
 export class NgclassComponent {
+  meuFavorito = false;
 
+  ativo = false;
+  tamanhoFonte = 10;
+
+  tarefa: any = {
+    desc: 'Descrição da tarefa',
+    responsavel: null,
+    responsavelElvis: {
+      usuário: null
+    }
+  }
+
+  onClick(){
+    this.meuFavorito = !this.meuFavorito;
+  }
+
+  mudarAtivo() {
+    this.ativo = !this.ativo;
+  }
 }
